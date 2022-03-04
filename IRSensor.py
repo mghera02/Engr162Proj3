5c306479195443ea4ed20da3db72c7f3724a7d2a
mghera02 <mghera02@hotmail.com>
2022-03-04 13:31:48 -0500

    IR Receiver Function/Files

---
+++ IRSensor.py
@@ -0,0 +1,20 @@
+import grovepi
+from IR_Functions import *
+
+IR_setup(grovepi)
+
+"""
+Returns a tuple of ints of the two values from the two IR sensor
+Paramaters: none
+"""
+def IRRead():
+    try:
+        [sensor1_value, sensor2_value]=IR_Read(grovepi)
+        
+        #print ("One = " + str(sensor1_value) + "\tTwo = " + str(sensor2_value))
+        return sensor1_value, sensor2_value
+
+    except IOError:
+        #print ("Error")
+        return "error"
+

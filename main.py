e175dd7b2d8eabfea635db30b9007ef3ec6c9254
mghera02 <mghera02@hotmail.com>
2022-03-20 15:37:47 -0400

    UNTESTED IR Sensor Analysis

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import motorDrive as motor
 import tSensor as touch
 import usRead as us
+import IRSensor as ir
 
 #-----------OTHER LIBRARY IMPORTS--------------#
 import time
@@ -11,6 +12,9 @@ desiredDistanceFromWall = 20
 currDirect = "forward"
 prevDirect = "stop"
 power = False
+minIRSensorVal = 20 # NEEDS TO BE CHANGED THIS IS JUST A RANDOM NUMBER
+IRObstacleDetected = False
+timeOfIRObstacle = 0
 
 #-------------HELPER FUNCTIONS-----------------#
 """
@@ -47,11 +51,19 @@ while True:
                 # On/off power using touch sensor as on/off button
                 power = touch.checkIfOn(power)
                 if power:
-                        #Detect walls in front and on sides
+                        # Detect IR beacons
+                        IRVal1, IRVal2 = ir.IRRead()
+                        if IRVal1 > minIRSensorVal or IRVal2 > minIRSensorVal and time.time() - timeOfIRObstacle > 10:
+                                IRObstacleDetected = True
+                                timeOfIRObstacle = time.time()
+                        else:
+                                IRObstacleDetected = False
+
+                        # Detect walls in front and on sides
                         wallDistLeft, wallDistFront, wallDistRight = us.ultraread(4,5,6)
-                        
                         wallsOnLeft, wallInFront, wallsOnRight = updateWall(wallDistLeft, wallDistRight, wallDistFront, desiredDistanceFromWall)
 
+                        # Direction changes
                         if (wallsOnLeft and wallsOnRight) and not wallInFront:# Keep going forward
                                 currDirect = "forward"
                         elif wallsOnRight and wallInFront and not wallsOnLeft: # Correcting to adjust left
